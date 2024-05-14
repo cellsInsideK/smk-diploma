bd7ec819f5a103274de35151a2326e815b839b61
cellsInsideK <thepevko@gmail.com>
2024-05-14 14:18:38 +0300

    diploma

---
+++ constants/index.ts
@@ -0,0 +1,44 @@
+export const sidebarLinks = [
+  {
+    imgURL: '/assets/home.svg',
+    route: '/',
+    label: 'Главная',
+  },
+  {
+    imgURL: '/assets/search.svg',
+    route: '/search',
+    label: 'Поиск',
+  },
+  {
+    imgURL: '/assets/heart.svg',
+    route: '/activity',
+    label: 'Уведомления',
+  },
+  {
+    imgURL: '/assets/create.svg',
+    route: '/create-thread',
+    label: 'Создать обсуждение',
+  },
+  // {
+  //   imgURL: '/assets/community.svg',
+  //   route: '/communities',
+  //   label: 'Сообщества',
+  // },
+  {
+    imgURL: '/assets/user.svg',
+    route: '/profile',
+    label: 'Профиль',
+  },
+];
+
+export const profileTabs = [
+  { value: 'threads', label: 'Обсуждения', icon: '/assets/reply.svg' },
+  { value: 'replies', label: 'Ответы', icon: '/assets/members.svg' },
+  { value: 'tagged', label: 'Упомянут', icon: '/assets/tag.svg' },
+];
+
+// export const communityTabs = [
+//   { value: 'threads', label: 'Обсуждения', icon: '/assets/reply.svg' },
+//   { value: 'members', label: 'Члены', icon: '/assets/members.svg' },
+//   { value: 'requests', label: 'Requests', icon: '/assets/request.svg' },
+// ];
